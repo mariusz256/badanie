926cfb9decec9daeff221b4a8a55f66dbec4892b
Mariusz <mariuszcyganik50@gmail.com>
2020-11-13 19:56:40 +0100

    End landing page

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -6,33 +6,54 @@ import { ReactComponent as Badania } from "../../assets/badania.svg";
 import { ReactComponent as Contactus } from "../../assets/contactus.svg";
 import { ReactComponent as Wspolpraca } from "../../assets/wspolpraca.svg";
 import { ReactComponent as Info } from "../../assets/info.svg";
+import { ReactComponent as Car } from "../../assets/car.svg";
+import { ReactComponent as Certyfikat } from "../../assets/certyfikat.svg";
+import { ReactComponent as Map } from "../../assets/map.svg";
+import { ReactComponent as Ok } from "../../assets/ok.svg";
+import { ReactComponent as Brain } from "../../assets/brain.svg";
 
 function HomePage() {
   return (
     <div className="HomePage">
       <div className="HomePage__wrapper">
         <section className="HomePage__content">
-          <h1>Badania Psychologiczne</h1>
+          <div className="HomePage__content__HeaderWrapper">
+            <Psi className="psi-svg" alt="Psi" />
+            <h1>Badania Psychologiczne</h1>
+          </div>
+          <div className="HomePage__text">
+            <Brain className="Icons" />
+            <p>
+              Badania psychologiczne z zakresu medycyny pracy i transportu
+              drogowego.
+            </p>
+          </div>
 
-          <p className="HomePage__text"></p>
-          <p className="HomePage__text">
-            {" "}
-            Badania psychologiczne z zakresu medycyny pracy i transportu
-            drogowego.
-          </p>
-          <p className="HomePage__text">
-            Nowoczesna aparatura do badań kierowców.
-          </p>
-          <p className="HomePage__text">Certyfikowane testy psychologiczne</p>
+          <div className="HomePage__text">
+            <Car className="Icons" />
+            <p>Nowoczesna aparatura do badań kierowców.</p>
+          </div>
 
-          <p className="HomePage__text">
-            Dajemy rękojmie jakości i rzetelności prowadzonych badań
-            psychologicznych.
-          </p>
-          <p className="HomePage__text">
-            Zapraszamy na badania psychologiczne klientów z Małopolski i Śląska.
-            Czekamy na Was w naszej pracowni psychologicznej.
-          </p>
+          <div className="HomePage__text">
+            <Certyfikat className="Icons" />
+            <p>Certyfikowane testy psychologiczne </p>
+          </div>
+
+          <div className="HomePage__text">
+            <Ok className="Icons" />
+            <p>
+              Dajemy rękojmie jakości i rzetelności prowadzonych badań
+              psychologicznych.
+            </p>
+          </div>
+
+          <div className="HomePage__text">
+            <Map className="Icons" />
+            <p>
+              Zapraszamy na badania psychologiczne klientów z Małopolski i
+              Śląska.
+            </p>
+          </div>
         </section>
 
         <section className="HomePage__Links">
@@ -50,7 +71,6 @@ function HomePage() {
           </Link>
         </section>
       </div>
-      <Psi className="psi-svg" alt="Psi" />
     </div>
   );
 }
